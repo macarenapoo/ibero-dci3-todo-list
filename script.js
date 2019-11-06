be0a2085023f4d6ae7ae1d4a47af9afab6d6282e
morado20 <morado20@morado20.id.dis.ibero.mx>
2019-11-06 09:25:34 -0600

    Final

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-// Script
+var pendientes = [
+    { descripcion: "Tarea de Agenda de JS", completado: false },
+    { descripcion: "Comer", completado: false },
+    { descripcion: "Dormir", completado: true },
+    { descripcion: "Cortarse el pelo", completado: false }
+];
+
+function imprimirPendiente(pendiente, index) {
+    var lista = document.getElementById("lista");
+    lista.insertAdjacentHTML('beforeend',
+        `<li ` + estaCompletado(pendiente.completado) + ` onClick="marcarCompletado(` + index + `)">
+            <div class="checkbox">
+                <i class="fa fa-check"></i>
+            </div>
+            <div class="descripcion">`+ pendiente.descripcion + `</div>
+            <i class="fa fa-times" onClick="eliminarPendiente(`+ index + `)"></i>
+        </li>`
+    );
+}
+
+function eliminarPendiente(index) {
+    pendientes.splice(index, 1);
+    imprimirTodosLosPendientes();
+}
+
+function estaCompletado(completado) {
+    if (completado) {
+        return 'class="done pendiente"';
+    } else {
+        return 'class="pendiente"';
+    }
+}
+
+function marcarCompletado(index) {
+    // var pendientesInterfaz = document.getElementsByClassName("pendiente");
+    // pendientesInterfaz[index].classList.toggle("done");
+    pendientes[index].completado = !pendientes[index].completado;
+    imprimirTodosLosPendientes();
+}
+
+function imprimirTodosLosPendientes() {
+    document.getElementById("lista").innerHTML = "";
+    pendientes.forEach(imprimirPendiente);
+}
+
+function agregarPendiente() {
+    var pendiente = document.getElementById("item").value;
+    pendientes.push({ descripcion: pendiente, completado: false });
+    imprimirTodosLosPendientes();
+}
+
+imprimirTodosLosPendientes();
